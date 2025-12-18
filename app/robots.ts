16689d0bdf8b00c46ec3eee5564fa1ea55a22d87
MohammadAmin <bluckb.com@gmail.com>
2025-12-18 19:55:31 +0000

    Fix robots.txt: allow /_next/static/ and /_next/image/ for Googlebot
    Googlebot was blocked from accessing JS, CSS, and optimized images
    which prevented proper page rendering during indexing.

--- app/robots.ts
+++ app/robots.ts
@@ -5,8 +5,8 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/_next/'],
+        allow: ['/', '/_next/static/', '/_next/image/'],
+        disallow: ['/api/'],
       },
     ],
     sitemap: 'https://mohammad.biz/sitemap.xml',
